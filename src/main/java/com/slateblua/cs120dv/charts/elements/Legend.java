82078924199164b755bde858b4403b94197ccf23
Sergey Hayriyan <sega.1ness@gmail.com>
2024-03-08 07:28:35 +0400

    Formatted

--- src/main/java/com/slateblua/cs120dv/charts/elements/Legend.java
+++ src/main/java/com/slateblua/cs120dv/charts/elements/Legend.java
@@ -5,9 +5,9 @@ import com.slateblua.cs120dv.core.Drawable;
 import java.awt.*;
 
 public class Legend implements Drawable {
+
     private final int WIDTH = 100;
     private final int HEIGHT = 100;
-
 
     @Override
     public void draw(Graphics g) {
